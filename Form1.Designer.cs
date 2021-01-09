593573d35ef8441a63bc97192e1ced12f540cf3a
Daniil Mardanov <wally19@bk.ru>
2021-01-10 01:35:08 +0400

    start panel template

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,14 +29,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.startPanel = new System.Windows.Forms.Panel();
+            this.startInput_name = new System.Windows.Forms.TextBox();
+            this.startInput_password = new System.Windows.Forms.TextBox();
+            this.registrationButton = new System.Windows.Forms.Button();
+            this.authorizationButton = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.profilePage = new System.Windows.Forms.TabPage();
             this.ticketsPage = new System.Windows.Forms.TabPage();
             this.registrationPage = new System.Windows.Forms.TabPage();
             this.historyPage = new System.Windows.Forms.TabPage();
             this.employeesPage = new System.Windows.Forms.TabPage();
+            this.startPanel.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // startPanel
+            // 
+            this.startPanel.Controls.Add(this.startInput_password);
+            this.startPanel.Controls.Add(this.startInput_name);
+            this.startPanel.Controls.Add(this.registrationButton);
+            this.startPanel.Controls.Add(this.authorizationButton);
+            this.startPanel.Location = new System.Drawing.Point(262, 150);
+            this.startPanel.Name = "startPanel";
+            this.startPanel.Size = new System.Drawing.Size(276, 158);
+            this.startPanel.TabIndex = 0;
+            // 
+            // startInput_name
+            // 
+            this.startInput_name.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startInput_name.Location = new System.Drawing.Point(0, 37);
+            this.startInput_name.Margin = new System.Windows.Forms.Padding(6);
+            this.startInput_name.Name = "startInput_name";
+            this.startInput_name.Size = new System.Drawing.Size(276, 26);
+            this.startInput_name.TabIndex = 1;
+            // 
+            // startInput_password
+            // 
+            this.startInput_password.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startInput_password.Location = new System.Drawing.Point(0, 75);
+            this.startInput_password.Margin = new System.Windows.Forms.Padding(6);
+            this.startInput_password.Name = "startInput_password";
+            this.startInput_password.Size = new System.Drawing.Size(276, 26);
+            this.startInput_password.TabIndex = 2;
+            // 
+            // registrationButton
+            // 
+            this.registrationButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.registrationButton.Location = new System.Drawing.Point(146, 110);
+            this.registrationButton.Name = "registrationButton";
+            this.registrationButton.Size = new System.Drawing.Size(130, 30);
+            this.registrationButton.TabIndex = 1;
+            this.registrationButton.Text = "Регистрация";
+            this.registrationButton.UseVisualStyleBackColor = true;
+            this.registrationButton.Click += new System.EventHandler(this.registrationButton_Click);
+            // 
+            // authorizationButton
+            // 
+            this.authorizationButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.authorizationButton.Location = new System.Drawing.Point(0, 110);
+            this.authorizationButton.Name = "authorizationButton";
+            this.authorizationButton.Size = new System.Drawing.Size(130, 30);
+            this.authorizationButton.TabIndex = 0;
+            this.authorizationButton.Text = "Авторизация";
+            this.authorizationButton.UseVisualStyleBackColor = true;
+            this.authorizationButton.Click += new System.EventHandler(this.authorizationButton_Click);
             // 
             // tabControl1
             // 
@@ -51,8 +108,8 @@
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.Padding = new System.Drawing.Point(20, 3);
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(776, 426);
-            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(760, 537);
+            this.tabControl1.TabIndex = 2;
             this.tabControl1.TabStop = false;
             this.tabControl1.Visible = false;
             // 
@@ -61,7 +118,7 @@
             this.profilePage.Location = new System.Drawing.Point(4, 27);
             this.profilePage.Name = "profilePage";
             this.profilePage.Padding = new System.Windows.Forms.Padding(3);
-            this.profilePage.Size = new System.Drawing.Size(768, 395);
+            this.profilePage.Size = new System.Drawing.Size(752, 506);
             this.profilePage.TabIndex = 0;
             this.profilePage.Text = "Профиль";
             this.profilePage.UseVisualStyleBackColor = true;
@@ -72,7 +129,7 @@
             this.ticketsPage.Location = new System.Drawing.Point(4, 27);
             this.ticketsPage.Name = "ticketsPage";
             this.ticketsPage.Padding = new System.Windows.Forms.Padding(3);
-            this.ticketsPage.Size = new System.Drawing.Size(768, 395);
+            this.ticketsPage.Size = new System.Drawing.Size(752, 506);
             this.ticketsPage.TabIndex = 1;
             this.ticketsPage.Text = "Тикеты";
             this.ticketsPage.UseVisualStyleBackColor = true;
@@ -81,7 +138,7 @@
             // 
             this.registrationPage.Location = new System.Drawing.Point(4, 27);
             this.registrationPage.Name = "registrationPage";
-            this.registrationPage.Size = new System.Drawing.Size(768, 395);
+            this.registrationPage.Size = new System.Drawing.Size(752, 506);
             this.registrationPage.TabIndex = 2;
             this.registrationPage.Text = "Заявки на регистрацию";
             this.registrationPage.UseVisualStyleBackColor = true;
@@ -90,7 +147,7 @@
             // 
             this.historyPage.Location = new System.Drawing.Point(4, 27);
             this.historyPage.Name = "historyPage";
-            this.historyPage.Size = new System.Drawing.Size(768, 395);
+            this.historyPage.Size = new System.Drawing.Size(752, 506);
             this.historyPage.TabIndex = 3;
             this.historyPage.Text = "История";
             this.historyPage.UseVisualStyleBackColor = true;
@@ -99,7 +156,7 @@
             // 
             this.employeesPage.Location = new System.Drawing.Point(4, 27);
             this.employeesPage.Name = "employeesPage";
-            this.employeesPage.Size = new System.Drawing.Size(768, 395);
+            this.employeesPage.Size = new System.Drawing.Size(752, 506);
             this.employeesPage.TabIndex = 4;
             this.employeesPage.Text = "Сотрудники";
             this.employeesPage.UseVisualStyleBackColor = true;
@@ -108,13 +165,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.startPanel);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.startPanel.ResumeLayout(false);
+            this.startPanel.PerformLayout();
             this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -122,6 +182,11 @@
 
         #endregion
 
+        private System.Windows.Forms.Panel startPanel;
+        private System.Windows.Forms.Button authorizationButton;
+        private System.Windows.Forms.Button registrationButton;
+        private System.Windows.Forms.TextBox startInput_name;
+        private System.Windows.Forms.TextBox startInput_password;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage profilePage;
         private System.Windows.Forms.TabPage ticketsPage;
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace TaxOffice
 {
@@ -7,7 +9,43 @@ namespace TaxOffice
         public Form1()
         {
             InitializeComponent();
-            tabControl1.Controls.Clear();
+            AddWaterMark(startInput_name, "ФИО");
+            AddWaterMark(startInput_password, "Пароль");
+        }
+
+        private void authorizationButton_Click(object sender, EventArgs e)
+        {
+            startPanel.Visible = false;
+            startPanel.Enabled = false;
+            tabControl1.Enabled = true;
+            tabControl1.Visible = true;
+        }
+
+        private void registrationButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void AddWaterMark(TextBox textBox, string watermarkText)
+        {
+            textBox.ForeColor = SystemColors.GrayText;
+            textBox.Text = watermarkText;
+            textBox.Leave += (o, e) =>
+            {
+                if (textBox.Text.Length == 0)
+                {
+                    textBox.Text = watermarkText;
+                    textBox.ForeColor = SystemColors.GrayText;
+                }
+            };
+            textBox.Enter += (o, e) =>
+            {
+                if (textBox.Text == watermarkText)
+                {
+                    textBox.Text = "";
+                    textBox.ForeColor = SystemColors.WindowText;
+                }
+            };
         }
     }
 }
